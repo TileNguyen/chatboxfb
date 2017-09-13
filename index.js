7b4e82a46bf81eddfa1dea792326ca5117e16d06
Nguyễn Ngói <nguyengoi1004@gmail.com>
2017-09-14 03:07:38 +0700

    listen request

--- index.js
+++ index.js
@@ -50,4 +50,8 @@ app.post('/', function (req, res) {
   });
 });
 
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
+
 module.exports = app;
