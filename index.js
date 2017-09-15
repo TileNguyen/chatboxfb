37a3879b7c5d4db3e4fabd16b9deb379a101eae2
Nguyễn Ngói <nguyengoi1004@gmail.com>
2017-09-15 09:32:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 1337));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
